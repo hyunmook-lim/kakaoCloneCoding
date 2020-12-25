30bd6c55b766e4cc01836bb1b8a1f517247ec424
yookjungsoo <alsue2000@gmail.com>
2020-12-26 00:39:14 +0900

    2020.12.26 00.39

---
+++ javascript/friends.js
@@ -0,0 +1,19 @@
+const search_button = document.querySelector('.search_button')
+const search_box = document.querySelector('.search_box')
+
+search_button.addEventListener('click', () => {
+	search_box.classList.toggle('active');
+})
+
+const bookmark_uparrow = document.querySelector('.bookmark_uparrow')
+const bookmark_list = document.querySelector('.bookmark_list')
+
+bookmark_uparrow.addEventListener('click', () => {
+	bookmark_list.classList.toggle('active');
+})
+
+const bookmark_arrow = document.
+
+if (bookmark_list.classList.toggle('nonactive')) {
+	bookmark_uparrow.nodeValue.classList
+}
